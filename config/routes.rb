ade1653088a5a8b72d0410372e3312b41c440793
Yoda1976 <t_braz02@hotmail.com>
2021-07-06 11:59:37 +0100

    Add signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
+  
+  # Routes for USERS
+  #to root/signup
+  get 'signup', to: 'users#new'
+  # Remaining CRUD operation
+  resources :users, except: [:new]
+  
+  # Routes for ARTICLES
   resources :articles
 end
